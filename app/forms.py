75fb5b025ae58b10f770704e7fb4797431463155
moba <m@b>
2014-02-06 23:37:23 +0100

    use Requred instead of InputRequired wtforms

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, DecimalField, HiddenField
-from wtforms.validators import InputRequired,Required
+from wtforms.validators import Required
 
 class ProjectForm(Form):
     name = TextField('name', validators = [Required()])
 
 class EntryForm(Form):
-    name = TextField('name', validators = [InputRequired()])
-    value = DecimalField('value', validators = [InputRequired()])
-    project_id = HiddenField('project_id', validators = [InputRequired()])
+    name = TextField('name', validators = [Required()])
+    value = DecimalField('value', validators = [Required()])
+    project_id = HiddenField('project_id', validators = [Required()])
